cbb2bc80e48d3d0359b199b91059f47948a13876
Evgeny Sushkov <evgeny.sushkov@apollointernet.io>
2024-06-10 22:29:26 +0200

    logoImageView_setting
    добавление лого

--- OS13_HW12/OS13_HW12/ViewController.swift
+++ OS13_HW12/OS13_HW12/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
     let workTime = 25
     let restTime = 5
     
+    let logoImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFit
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.image = UIImage(named: "logo") // Замените "logo" на имя вашего логотипа в Assets
+            return imageView
+        }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()

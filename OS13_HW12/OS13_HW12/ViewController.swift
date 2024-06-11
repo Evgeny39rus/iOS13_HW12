b5ae45428a1c6f73a80b93f3ada716809bc73a1e
Evgeny Sushkov <evgeny.sushkov@apollointernet.io>
2024-06-11 22:23:16 +0200

    add new methods and snapkit
    add new methods and snapkit

--- OS13_HW12/OS13_HW12/ViewController.swift
+++ OS13_HW12/OS13_HW12/ViewController.swift
@@ -6,30 +6,91 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController, CAAnimationDelegate {
+    private var isWorkTime = true
+    private var isStarted = false
+    private var isAnimationStarted = false
+    private var timer = Timer()
+    private let workTime = 25
+    private let restTime = 10
+    private var time = 25
     
-    var isWorkTime = true
-    var isStarted = false
-    var timer: Timer?
-    let workTime = 25
-    let restTime = 5
+    private let foregroundProgressLayer = CAShapeLayer()
+    private let backgroundProgressLayer = CAShapeLayer()
     
-    let logoImageView: UIImageView = {
-            let imageView = UIImageView()
-            imageView.contentMode = .scaleAspectFit
-            imageView.translatesAutoresizingMaskIntoConstraints = false
-            imageView.image = UIImage(named: "logo") // Замените "logo" на имя вашего логотипа в Assets
-            return imageView
-        }()
+    // MARK: - Outlets
     
+    private lazy var modeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Work"
+        label.textAlignment = .center
+        label.textColor = .label
+        label.font = UIFont.systemFont(ofSize: 40, weight: .bold)
+        return label
+    }()
     
-
+    private lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00:25"
+        label.textAlignment = .center
+        label.textColor = .label
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        return label
+    }()
+    
+    private var startPauseButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.addTarget(self, action: #selector(startPausePressed), for: .touchUpInside)
+        button.setImage(getImage("play"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        return button
+    }()
+    
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        drawBackgroundLayer()
+        setupStackView()
+        setupHierarchy()
+        setupLayout()
     }
-
-
+    
+    private func setupStackView() {
+        stackView.addArrangedSubview(modeLabel)
+        stackView.addArrangedSubview(timerLabel)
+        stackView.addArrangedSubview(startPauseButton)
+    }
+    
+    private func setupHierarchy() {
+        view.addSubviews(stackView)
+    }
+    
+    private func setupLayout() {
+        stackView.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalToSuperview().offset (100)
+            make.bottom.equalToSuperview() .offset(-100)
+        }
+    }
+    
+    private func drawBackgroundLayer() {
+        backgroundProgressLayer.path = UIBezierPath(
+            arcCenter: CGPoint(x: view.frame.midX, y: view.frame.midY),
+            radius: 150, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true
+        ).cgPath
+        backgroundProgressLayer.strokeColor = UIColor.lightGray.cgColor
+        backgroundProgressLayer.fillColor = UIColor.clear.cgColor
+        backgroundProgressLayer.lineWidth = 10
+        view.layer.addSublayer(backgroundProgressLayer)
+    }
 }
-
